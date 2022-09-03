6d3d91ba0b2873b02f01cb0033b3a36e931faece
MxnWgd <maxiw@outlook.com>
2022-09-03 17:40:11 +0200

    Ver 1.1.2 - animation fine tuning - style fine tuning - restyled post blocks

--- inc/help/help_persons.php
+++ inc/help/help_persons.php
@@ -1,4 +1,5 @@
 <h1>Personen</h1>
+<img src="<?php echo get_template_directory_uri(); ?>/inc/help/img/persons1.png" alt="Personenkachel" style="width: 100%; max-width: 800px;">
 <p>Über den Menüpunkt <strong>Personen</strong> können verschiedene Personen mit Namen, Position, Kurzbeschreibung, Bild etc. angelegt werden. Auch E-Mail-Adressen und Social-Media-Kanäle können hinterlegt werden.</p>
 <p>Aus den Informationen generiert das Theme vorgefertigte Personenkacheln, die auf beliebigen Seiten eingefügt werden können.</p>
 
@@ -9,5 +10,6 @@
 <h3>Einfügen einer Person auf einer Seite</h3>
 <p>Personen werden nach ihrer Erstellung über sogenannte Shortcodes auf beliebigen Seiten einfügt.</p>
 <p>Hierzu muss an der entsprechenden Stelle der Shortcode <code>[person id="<em>ID</em>"]</code> eingefügt werden, wobei die auf der Bearbeitungsseite angezeigte ID der jeweiligen Person eingefügt werden muss. Auf dieser befindet sich auch ein bereits fertiger Shortcode, der lediglich kopiert und eingefügt werden muss.</p>
+<img src="<?php echo get_template_directory_uri(); ?>/inc/help/img/persons2.png" alt="Shortcodes" style="width: 300px;">
 <p>Auf diesem Wege lassen sich bspw. mehrere Personen auf Vorstands- oder Mandatsträgerseiten zusammenfügen.</p>
 <p>Die IDs werden ebenfalls verwendet, um Personen in der Personenliste auf der Startseite anzuzeigen. Weitere Informationen hierzu findest du in der <a href="<?php menu_page_url('help_settings'); ?>" title="Hilfeseite zu den Themeeinstellungen">Hilfe zu den Themeeinstellungen</a>.</p>
