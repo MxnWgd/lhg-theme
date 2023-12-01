0a4c6e12d9397fa5f405463bd686b0a2b2df5646
MxnWgd <maxiw@outlook.com>
2023-12-01 13:06:34 -0600

    Ver 2.5: - improved indent on sublists (#227) - generalized default link for newsletter btn in flyout (#232) - made event page be turned on by default (#231) - blocked filter change on load of posts (#226) - fixed link to news category on front page not working (#229) - fixed layout on no news (#222) - fixed editor List bug (#225) - code improvements

--- inc/help/help.php
+++ inc/help/help.php
@@ -17,7 +17,7 @@
       <code>
         <ul>
           <li>
-            <strong>Ver 2.5</strong><br>
+            <strong>Ver 2.5 </strong><br>
             - improved indent on sublists (#227)<br>
             - generalized default link for newsletter btn in flyout (#232)<br>
             - made event page be turned on by default (#231)<br>
